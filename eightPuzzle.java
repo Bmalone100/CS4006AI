9cfdb796ce0b4405155f2f89f5731ec249140432
Bmalone100 <36305546+Bmalone100@users.noreply.github.com>
2019-03-25 15:26:37 +0000

    okay
    yes

--- eightPuzzle.java
+++ eightPuzzle.java
@@ -1,71 +1,37 @@
 import javax.swing.JOptionPane;
+import java.util.*;
+
 public class ISProject
 {
 	public static void main(String [] args)
 	{   
         String puzzleChoice="";
-		String pattern = "[0-8]{0,9}";
-		String pattern2 = "[0-8]{0,15}";
-        String startState="";
-        String endState="";
-		String[] options = {"8 Puzzel", "15 Puzzel"};
-        puzzleChoice = JOptionPane.showInputDialog(null, "Choose a puzell", "Menu", 
+		String pattern = "0123456789";
+        String StartState="";
+        String EndState="";
+		String[] options = {"8 Puzzle", "15 Puzzle"};
+        puzzleChoice = JOptionPane.showInputDialog(null, "Choose a puzzle", "Menu", 
 		JOptionPane.PLAIN_MESSAGE, null, options, options[0]).toString();
 		if(puzzleChoice==options[0])
 		{
-			eightPuzzle(pattern,startState,endState);
+			JOptionPane.showMessageDialog(null,"eight puzzle?,ok");
+			//eightPuzzle();
 		}
 		else
 		{
-			
-			fifteenPuzzle(pattern2,startState,endState);
+			JOptionPane.showMessageDialog(null,"fifteen puzzle?,ok");
+			//fifteenPuzzle();
 		}
 	}
 	//Ibrahim
-	public static void eightPuzzle(String pattern, String startState, String endState)
+	public static void eightPuzzle()
 	{
-		int tries = 0;
-		while(tries<3){
-				startState = JOptionPane.showInputDialog(null,"please enter all numbers from 0-9 as a start state");
-				endState = JOptionPane.showInputDialog(null,"please enter all numbers from 0-9 as an end state");
 		
-				if(startState.matches(pattern) && endState.matches(pattern))
-				{
-					String []startArray=startState.split("");
-					String [] endArray =endState.split("");
-					Validation(startArray,endArray);
-				}
-				else
-				{
-				JOptionPane.showMessageDialog(null,"we want a digit please from 0-9");
-				tries++;
-				}
-			 }
-			 JOptionPane.showMessageDialog(null,"you are clearly messing around i don't have time for dis");
-	
 	}
 	//Ibrahim
-	public static void fifteenPuzzle(String pattern2, String startState, String endState)
-	{	
-		int tries = 0;
-		while(tries<3){
-				startState = JOptionPane.showInputDialog(null,"please enter all numbers from 0-9 as a start state");
-				endState = JOptionPane.showInputDialog(null,"please enter all numbers from 0-9 as an end state");
+	public static void fifteenPuzzle()
+	{
 		
-				if(startState.matches(pattern2) && endState.matches(pattern2))
-				{
-					String []startArray=startState.split("");
-					String [] endArray =endState.split("");
-					Validation(startArray,endArray);
-					tries =3;
-				}
-				else
-				{
-				JOptionPane.showMessageDialog(null,"we want a digit please from 0-9");
-				tries++;
-				}
-			 }
-			 JOptionPane.showMessageDialog(null,"you are clearly messing around i don't have time for dis");
 	}
 	//daire
 	public static void Validation()
