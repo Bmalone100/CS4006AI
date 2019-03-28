d65fe6030f6fb4754c80ca63a2259938f9fb3661
dairelavelle1 <16192249@studentmail.ul.ie>
2019-03-28 16:17:31 +0000

    Update Puzzle.java

--- Puzzle.java
+++ Puzzle.java
@@ -3,8 +3,10 @@ import java.util.*;
 public class Puzzle {
     static Matrix startState= new Matrix();
     static Matrix endState= new Matrix();
-    ArrayList<Matrix> closed=new ArrayList<Matrix>();
-    ArrayList<Matrix> open=new ArrayList<Matrix>();
+    static Matrix currentState=new Matrix();
+    static ArrayList<Matrix> closed=new ArrayList<Matrix>();
+    static ArrayList<Matrix> open=new ArrayList<Matrix>();
+    static int puzzleSize=0;
 
     public static void main(String [] args)
     {
@@ -23,6 +25,7 @@ public class Puzzle {
 
             if(puzzleChoice.equals(options[0]))
             {
+            	puzzleSize=8;
                 eightPuzzle();
             }
             else
@@ -35,75 +38,70 @@ public class Puzzle {
     {
     	startState=new Matrix(8);
     	endState=new Matrix(8);
+    	currentState=new Matrix(8);
         String startStateIn = "";
         String endStateIn = "";
 
-        Scanner keyboard = new Scanner(System.in);
-
         startStateIn = JOptionPane.showInputDialog(null, "Input a starting puzzle state");
+        startStateIn+=" ";
+        //call validation here
         endStateIn = JOptionPane.showInputDialog(null, "Input an ending puzzle state");
+        endStateIn+=" ";
+        //call validation here
 
         String value [] = startStateIn.split(" ");
         String endValue [] = endStateIn.split(" ");
-        for (int i=0;i<8;i++) {
-            for (int j=0; j<3; j++) {
-                for (int k=0;k<3;k++) {
-                    startState.nodes[i]=new Node(Integer.parseInt(value[i]),j,k);
-                    endState.nodes[i]=new Node(Integer.parseInt(endValue[i]),j,k);
-                }
+        int i=0;
+        for (int j=0; j<3; j++) {
+            for (int k=0;k<3;k++) {
+                startState.nodes[i]=new Node(Integer.parseInt(value[i]),j,k);
+                currentState.nodes[i]=new Node(Integer.parseInt(value[i]),j,k);
+                endState.nodes[i]=new Node(Integer.parseInt(endValue[i]),j,k);
+                i++;
             }
         }
+        //this will loop
+        System.out.println(currentState);
+        int[] currentMoves=currentState.findZero(puzzleSize);
+        for (int j=0; j<currentMoves.length; j++) {
+        	if (currentMoves[j]!=0) {
+        		open.add(Matrix.newMovedMatrix(currentState, currentMoves[j]));
+        	}
+        }
+        for (int j=0; j<open.size(); j++) {
+        	System.out.println(open.get(j));
+        	System.out.println(calculateHeuristicValue(open.get(j)));
+        }
+
     }
 
-    public int calculateHeuristicValue(Matrix currentState)
+    public static int calculateHeuristicValue(Matrix in)
     {
         int totalHeuristic=0;
         //Assign Nodes values
         //Compare Start State vs End State 
         //Calculate h value for each node and assign to node
-        for (int i=0;i<currentState.getSize();i++) {
-            totalHeuristic+=Math.abs(currentState.nodes[i].getHeursitic()-endState.nodes[i].getHeursitic());
+        for (int i=0;i<in.getSize();i++) {
+        	System.out.println(String.format("%d %d %d %d %d %d",
+        		in.getNode(i).getValue(),
+        		in.getNode(i).getPositionX(),
+        		in.getNode(i).getPositionY(),
+        		endState.getNode(i).getValue(),
+        		endState.getNode(i).getPositionX(),
+        		endState.getNode(i).getPositionY()));
+            totalHeuristic+=Math.abs((in.getNode(i).getPositionX()-endState.getNode(i).getPositionX())+(in.getNode(i).getPositionX()-endState.getNode(i).getPositionY()));
+            System.out.println(String.format("Heuristic for %d is %d",i,Math.abs((in.getNode(i).getPositionX()-endState.getNode(i).getPositionX())+(in.getNode(i).getPositionX()-endState.getNode(i).getPositionY()))));
         }
-        return totalHeuristic+currentState.getG();
+        return totalHeuristic+in.getG();
     }
-            /****Iterate through start matrix and output initial puzzle state****/
 
-     void outputStartState(Matrix startState){
-        for(int i = 0; i<startState.getSize(); i++){
-        System.out.print(startState.nodes[i]);
-        if(i==3){System.out.print("\n");}
-        if(i==6){System.out.print("\n");}
-            }
-        }  
-             /****Iterate through current instance of matrix and output puzzle state****/
-     void outputCurrentState(Matrix currentState){
-        for(int i = 0; i<currentState.getSize(); i++){
-        System.out.print(currentState.nodes[i]);
-        if(i==3){System.out.print("\n");}
-        if(i==6){System.out.print("\n");}
-            }
-        } 
+    void calculateMovements (Matrix currentState) {
 
-    //takes the current state as a parameter and checks if it is present in the list of closed states
-	//returns TRUE if a duplicate IS FOUND
- //    public static boolean isDuplicate(Matrix currentState) {
-	// 	int dupeCount=0;
-	// 	boolean dupe=false;
-	// 	for(int i=0; i<closed.get(i).nodes.length; i++) {
-	// 		for (int j=0; j<closed.get(i).nodes.length; j++) {
-	// 			for (int k=0; k<closed.get(i)nodes.length; k++) {
-	// 				if (closed.get(i)[j][k]==currentState[j][k])
-	// 					dupeCount++;
-	// 			}
-	// 		}
-	// 		if (dupeCount>7) dupe= true;
-	// 	}
-	// 	return dupe;
-	// }
+    }
 }
 // ****************************************
 //              Private classes
-//****************************************
+//*****************************************
 
 class Node{
     int val; //numeric value
@@ -120,27 +118,40 @@ class Node{
     public int getValue() 	  {return val;}
     public int getPositionX() {return posx;}
     public int getPositionY() {return posy;}
-    public int getHeursitic() {return posx+posy;}
 
     public void setValue(int value) 	   {val = value;}
     public void setPositionX(int position) {posx = position;}
     public void setPositionY(int position) {posy = position;}
+
+    //Checks if the zero node can move or if it is against a side
+    //reuturns false for moves that can't be made
+    //the array is in {left, up, right, down}
+    public boolean[] findMoves(int puzzleSize) {
+    	int width=(int)Math.sqrt(puzzleSize+1)-1;
+    	boolean[] out={true, true, true, true};
+    	if (posy<1)
+    		out[0]=false;
+    	if (posx<1)
+    		out[1]=false;
+    	if (posy>width-1)
+    		out[2]=false;
+    	if (posx>width-1)
+    		out[3]=false;
+    	return out;
+    }
 }
 
 class Matrix {
     private int g=0; //gn from the brief
     private int size=0;
+    private int width=0;
     public 	Node nodes[]=new Node[9];
-
     //constructors
     public Matrix(){}
     public Matrix(int squares) { //creates the matrix for an 8 or 15 puzzle
     	nodes=new Node[squares+1];
-    	size=squares;
-    	//int width=Math.sqrt(squares+1)-1;
-        // for(int i=0;i<width;i++){
-        //     nodes[i]=new Node(in[i].getValue(), in[i].getPositionX(), in[i].getPositionY());
-        // }
+    	size=squares+1;
+    	width=(int)Math.sqrt(squares+1);
     }
 
     //getters and setters
@@ -148,7 +159,26 @@ class Matrix {
     //just use Mastrix.nodes[], for example startState.nodes[1]
     public int 	getSize() {return size;}
     public int 	getG() {return g;}
+    public Node getNode(int in) {
+    	boolean found=false;
+    	for(int i=0; i<size && !found; i++) {
+    		if (nodes[i].getValue()==in) {
+    			return nodes[i];
+    		}
+    	}
+    	return nodes[0];
+    }
+    public int findNode(int in) {
+    	boolean found=false;
+    	for(int i=0; i<nodes.length; i++) {
+    		if (in==nodes[i].getValue())
+    			return i;
+    	}
+    	return 0;
+    }
+
     public void setG(int in) {g=in;}
+    public void plusG() {g++;}
 
     //methods
     boolean isEqual(Matrix in) {
@@ -158,5 +188,62 @@ class Matrix {
 				out=false;
 		}
 		return out;
+    }
+    @Override
+    public String toString() {
+    	String out="";
+    	int index=0;
+    	for(int i=0; i<width;i++) {
+    		for (int j=0;j<width;j++) {
+    			out+=nodes[index].getValue()+" ";
+    			index++;
+    		}
+    		out+="\n";
+    	}
+    	return out;
+    }
+    public int[] findZero(int puzzleSize) {
+    	boolean found=false;
+    	int[] out=new int[4];
+    	for(int i=0; i<size && !found; i++) {
+    		if (nodes[i].getValue()==0) {
+    			found=true;
+    			boolean[] moves=nodes[i].findMoves(puzzleSize);
+				if (moves[0]==true) {
+					System.out.println(nodes[i-1].getValue()+" to the east\n");
+					out[0]=nodes[i-1].getValue();
+				}
+				if (moves[1]==true) {
+					System.out.println(nodes[i-width].getValue()+" to the south\n");
+					out[1]=nodes[i-width].getValue();
+				}
+
+				if (moves[2]==true) {
+					System.out.println(nodes[i+1].getValue()+" to the west\n");
+					out[2]=nodes[i+1].getValue();
+				}
+				if (moves[3]==true) {
+					System.out.println(nodes[i+width].getValue()+" to the north\n");
+					out[3]=nodes[i+width].getValue();
+				}
+    		}
+    	}
+    	return out;
+    }
+    public static Matrix newMovedMatrix(Matrix old, int in) {
+    	int zeroLocation=old.findNode(0);
+    	int inLocation=old.findNode(in);
+    	Node tempZero=new Node(0,old.getNode(0).getPositionX(),old.getNode(0).getPositionY());
+    	Node tempIn=new Node(in,old.getNode(in).getPositionX(),old.getNode(in).getPositionY());
+    	Matrix out=new Matrix(8); //HARDCODED
+    	for (int i=0; i<9; i++) {
+    		if (i!=zeroLocation && i!=inLocation)
+    			out.nodes[i]=new Node(old.nodes[i].getValue(), old.nodes[i].getPositionX(), old.nodes[i].getPositionY());
+    		else if (i==zeroLocation)
+    			out.nodes[i]=tempIn;
+    		else
+    			out.nodes[i]=tempZero;
+    	}
+    	return out;
     }
 }
